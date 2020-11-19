82a637f3a2169ddddf85bfd2fdedff08dde919fc
Alex Baretsky <41ax@mail.ru>
2020-11-20 00:17:03 +0400

    Update instagram contact

--- src/constants/contacts.ts
+++ src/constants/contacts.ts
@@ -2,7 +2,7 @@ export default {
   EMAIL: 'hi@sketchninjas.art',
   TELEGRAM: 'https://t.me/catunic',
   WHATSAPP: 'https://wa.me/79061494998',
-  INSTAGRAM: 'https://www.instagram.com/catunic',
+  INSTAGRAM: 'https://www.instagram.com/sketchninjas/',
   BEHANCE: 'https://www.behance.net/catunic',
   DRIBBBLE: 'https://dribbble.com/catunic',
 };
